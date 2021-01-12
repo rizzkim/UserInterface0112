377fa9ac4bafa41ffe1e1ff61008278ccc9e0012
Dohyung Kim <rizz@Dohyungui-MacBookPro.local>
2021-01-12 18:21:47 +0900

    21.01.12 Education

---
+++ app/src/main/java/com/example/userinterface0112/ButtonActivity.kt
@@ -0,0 +1,17 @@
+package com.example.userinterface0112
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_button.*
+
+class ButtonActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_button)
+
+        //버튼 누를 때 수행할 코드
+        btn.setOnClickListener{
+            text.text="버튼을 누름"
+        }
+    }
+}
